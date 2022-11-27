9f0623aa65a60bd3e2a2b2447807a664b5f7c608
Vlad Bulgakov <stick.qwe@gmail.com>
2022-11-28 00:31:48 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="grid grid-cols-4 gap-4">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+        <div>04</div>
+        <div>05</div>
+        <div>06</div>
+        <div>07</div>
+        <div>08</div>
+        <div>09</div>
+        <div>10</div>
+        <div>11</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
