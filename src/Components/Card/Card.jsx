47e62cc5174adda096cf92e284968343dfd9d745
Vlad Bulgakov <stick.qwe@gmail.com>
2022-11-28 16:32:21 +0300

    change view

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -17,7 +17,6 @@ function Card({ product }) {
         <div className="mt-4 flex justify-between">
           <div className="w-full">
             <h3 className="text-sm text-gray-700">
-                <span aria-hidden="true" className="absolute inset-0"></span>
                 { product.name }
             </h3>
             <button type="button" onClick={onCheckout} className="py-2.5 w-full px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Выбрать</button>
